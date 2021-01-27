31462cc6f02373a7a9d446c9d6e9eea17b4c5390
Miku <anujsreedharan@aol.com>
2021-01-28 00:41:59 +0530

    add files

---
+++ README.md
@@ -0,0 +1,9 @@
+# AL-Notifier
+
+## A python script to send webhooks for notification's for anilist.
+
+If you want to use it clone the repo and change the user name, Im too lazy lol.
+
+##### Note: Using docker isn't necessary, I'll just add the step's if someone ever want's to use this. 
+
+Feel free to dm me on discord kazuma#0002.
---
+++ notifier.py
@@ -0,0 +1,55 @@
+import requests, os
+from discord_webhook import DiscordWebhook
+from time import sleep as wait
+
+userTOKEN = os.environ["userToken"]
+userID = os.environ["userID"]
+userName = os.environ["userName"]
+def headerReturn():
+    return {
+        "Authorization": "Bearer " + userTOKEN,
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    }
+
+
+def queryReturn(): #too lazy to add variables for username, should set it later.
+    return {
+        "query": """query{
+            User(name:"kazuma"){ 
+            unreadNotificationCount
+        }
+    }""",
+    }
+
+
+def sendWebhook(content):
+    hook = DiscordWebhook(
+        url=os.environ["webhookLink"], content=content, username="Anilist"
+    )
+    hook.execute()
+
+
+currentCount = 0
+
+
+def sendRequest(count):
+    global currentCount
+    requestObject = requests.post(
+        "https://graphql.anilist.co", json=queryReturn(), headers=headerReturn()
+    )  # object to recieve requests.
+    presentCount = requestObject.json()["data"]["User"]["unreadNotificationCount"]
+    if presentCount != currentCount:
+        sendWebhook(
+            f"<@!{userID}> You have {presentCount} unread notifications, {count}'th request."
+        )
+        currentCount = presentCount
+    else:
+        pass
+
+
+count = 0
+while True:
+    count += 1
+    sendRequest(count)
+    wait(60)
